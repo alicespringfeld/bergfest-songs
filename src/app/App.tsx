9d4b3e639151dec5d2dc0da96404d736822d9a1f
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-03 12:04:31 +0100

    :sparkles: Add HTML code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import styles from './app.module.css';
 
 function App(): JSX.Element {
-  return <div>Welcome to Bergfest! 🍻</div>;
+  return (
+    <main className={styles.container}>
+      <div>
+        <h1 className={styles.title}>Welcome to Bergfest! 🍻</h1>
+        <form className={styles.form}>
+          <input
+            type="text"
+            className={styles.form__field}
+            placeholder="First name"
+          />
+          <input
+            type="text"
+            className={styles.form__field}
+            placeholder="Last name"
+          />
+          <input type="submit" className={styles.form__button} />
+        </form>
+      </div>
+    </main>
+  );
 }
 export default App;
